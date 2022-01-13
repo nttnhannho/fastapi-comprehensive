1985b82a0384cec3651f6659ce4ecb8a62d89920
nttnhannho <makelifeperfect@gmail.com>
2022-01-13 21:46:15 +0700

    Creating; Retrieve one

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
+from random import randrange
 
 
 app = FastAPI()
@@ -27,6 +28,12 @@ my_posts = [
 ]
 
 
+def find_post(id_):
+    for p in my_posts:
+        if p["id"] == id_:
+            return p
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -37,6 +44,15 @@ async def get_posts():
     return {"data": my_posts}
 
 
+@app.get("/posts/{id_}")
+async def get_post(id_: int):
+    post = find_post(id_)
+    return {"post_detail": post}
+
+
 @app.post("/posts")
 async def create_posts(post: Post):
-    return {"data": post}
+    post_dict = post.dict()
+    post_dict["id"] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
