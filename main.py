7dfb8d6e530f516de60447b8bf021b31b41aa93f
nttnhannho <makelifeperfect@gmail.com>
2022-01-13 20:19:18 +0700

    Starting FastAPI; Path operations; Postman; HTTP Requests; Schema validation with Pydantic

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-def solution():
-    return "Hello, Nhan"
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 
-def main():
-    print(solution())
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+class Post(BaseModel):
+    title: str
+    content: str
+    is_published: bool = True
+    rating: Optional[int] = None
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/posts")
+async def get_posts():
+    return {"data": "This is your post"}
+
+
+@app.post("/posts")
+async def create_post(post: Post):
+    return {"data": post}
